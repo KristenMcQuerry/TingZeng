954ffc303bf95bb059cbb00e6fb9ffafad5365e6
Zeng <tze225@uky.edu>
2018-02-27 11:25:51 -0500

    changes after clarification

--- 693 Spring 2018 Assignment 1.R
+++ 693 Spring 2018 Assignment 1.R
@@ -12,7 +12,7 @@ flights
 
 # part a: arr_delay>=2
 flights %>% 
-  filter(arr_delay>=2)
+  filter(arr_delay>=120)
 
 # part b: flew to Houston, IAH or HOU
 flights %>% 
@@ -28,11 +28,11 @@ flights %>%
 
 # part e: arr_delay>2,but didn't leave late
 flights %>% 
-  filter(arr_delay>2,dep_delay<=0)
+  filter(arr_delay>120,dep_delay<=0)
 
 # part f: were delayed at least 1 hr but make up 30 mins in flight
 flights %>% 
-  filter(dep_delay>=1,dep_delay-arr_delay>=30)
+  filter(dep_delay>=60,dep_delay-arr_delay>=30)
 
 # part g: departed b/t midnight and 6 a.m.(inclusive)
 flights %>% 
@@ -47,10 +47,13 @@ flights %>%
   arrange(dep_delay)
 
 # part i:sort to find the fastest flights
- flights %>% 
-   mutate(speed=arr)
-
-
-# part j: which flights traveled the longest?
 flights %>% 
-  arrange(desc(distance))
+   mutate(speed=distance/air_time) %>% 
+   arrange(desc(speed))
+
+# part j: which flights traveled the longest/shortest?
+flights %>% 
+  arrange(desc(distance)) #longest
+
+flights %>% 
+  arrange(distance) #shortest
